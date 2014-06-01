5e577fe217f56ca8063f8917d6c43a5908db04d7
natoehv <nato.ehv@gmail.com>
2014-06-01 02:38:04 -0400

    Se agregan valores de bootstrap para la responsividad

--- index.php
+++ index.php
@@ -10,57 +10,57 @@
 
     </head>
     <body>
-        <header id="header">
+        <header id="header" class="row">
             Area header
         </header>
-
-        <nav id="nav">
+        <nav id="nav" class="row">
             Area nav
         </nav>
+		<div class="row">
+			<aside class="col-md-3">
+				Area aside
+				<ul class="nav nav-pills nav-stacked accordion">
+					<li id="one" class="files">
+						<a href="#one">My Files<span>495</span></a>
+						<ul class="sub-menu">
+							<li><a href="#one"><em>01</em>Dropbox<span>42</span></a></li>
+							<li><a href="#one"><em>02</em>Skydrive<span>87</span></a></li>
+							<li><a href="#one"><em>03</em>FTP Server<span>366</span></a></li>
+						</ul>
+					</li>
+					<li id="two" class="mail">
+						<a href="#two">Mail<span>26</span></a>
+						<ul class="sub-menu">
+							<li><a href="#two"><em>01</em>Hotmail<span>9</span></a></li>
+							<li><a href="#two"><em>02</em>Yahoo<span>14</span></a></li>
+							<li><a href="#two"><em>03</em>Gmail<span>3</span></a></li>
+						</ul>
+					</li>
+					<li id="three" class="cloud">
+						<a href="#three">Cloud<span>58</span></a>
+						<ul class="sub-menu">
+							<li><a href="#three"><em>01</em>Connect<span>12</span></a></li>
+							<li><a href="#three"><em>02</em>Profiles<span>19</span></a></li>
+							<li><a href="#three"><em>03</em>Options<span>27</span></a></li>
+						</ul>
+					</li>
+					<li id="four" class="sign">
+						<a href="#four">Sign Out</a>
+						<ul class="sub-menu">
+							<li><a href="#four"><em>01</em>Log Out</a></li>
+							<li><a href="#four"><em>02</em>Delete Account</a></li>
+							<li><a href="#four"><em>03</em>Freeze Account</a></li>
+						</ul>
+					</li>
+				</ul>
+			</aside>
 
-        <aside id="aside">
-            Area aside
-            <ul class="accordion">
-                <li id="one" class="files">
-                    <a href="#one">My Files<span>495</span></a>
-                    <ul class="sub-menu">
-                        <li><a href="#one"><em>01</em>Dropbox<span>42</span></a></li>
-                        <li><a href="#one"><em>02</em>Skydrive<span>87</span></a></li>
-                        <li><a href="#one"><em>03</em>FTP Server<span>366</span></a></li>
-                    </ul>
-                </li>
-                <li id="two" class="mail">
-                    <a href="#two">Mail<span>26</span></a>
-                    <ul class="sub-menu">
-                        <li><a href="#two"><em>01</em>Hotmail<span>9</span></a></li>
-                        <li><a href="#two"><em>02</em>Yahoo<span>14</span></a></li>
-                        <li><a href="#two"><em>03</em>Gmail<span>3</span></a></li>
-                    </ul>
-                </li>
-                <li id="three" class="cloud">
-                    <a href="#three">Cloud<span>58</span></a>
-                    <ul class="sub-menu">
-                        <li><a href="#three"><em>01</em>Connect<span>12</span></a></li>
-                        <li><a href="#three"><em>02</em>Profiles<span>19</span></a></li>
-                        <li><a href="#three"><em>03</em>Options<span>27</span></a></li>
-                    </ul>
-                </li>
-                <li id="four" class="sign">
-                    <a href="#four">Sign Out</a>
-                    <ul class="sub-menu">
-                        <li><a href="#four"><em>01</em>Log Out</a></li>
-                        <li><a href="#four"><em>02</em>Delete Account</a></li>
-                        <li><a href="#four"><em>03</em>Freeze Account</a></li>
-                    </ul>
-                </li>
-            </ul>
-        </aside>
-
-        <section id="section">
-            Area section
-            <article class="borde" id="article">
-                Area article
-            </article>
-        </section>
+			<section  class="col-md-9">
+				Area section
+				<article class="borde" id="article">
+					Area article
+				</article>
+			</section>
+		</div>
     </body>
 </html>
