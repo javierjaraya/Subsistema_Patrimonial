93e80e4dda12771c05dc4c770103c8ba72c95f11
natoehv <nato.ehv@gmail.com>
2014-06-01 13:28:17 -0400

    Propuesta de js para manejar los eventos dentro de la página

---
+++ assets/js/eventos.js
@@ -0,0 +1,31 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ * 
+ * Propuesta uso de javascript en subsistema_patrimonio
+ * @type eventos._L10.Anonym$0
+ */
+var eventos = (function() {
+  var variablePublica = "podria iniciar un widget aqui";
+
+  return {
+    /**
+     * 
+     */
+    cargarTabla: function(tabla) {
+      console.log('tabla cargada');
+    },
+    /**
+     * 
+     * @param {type} people
+     * @returns {undefined}
+     */
+    vaciaTabla: function(tabla) {
+      console.log('tabla ocultada');
+    },
+  };
+})();
