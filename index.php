af184791df496d57d9426dc2df4f6cbc626a6c53
Javier <Javier@Javier-PC>
2014-06-01 03:23:24 -0400

    Version 1

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
         <link href='/assets/ico/favicon.ico' type='image/x-icon' rel='shortcut icon' />
         <!-- Bootstrap core CSS -->
         <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
-        
+
 
     </head>
     <body>
@@ -16,58 +16,58 @@
                 Area header
             </header>
         </div>
-        
+
         <div class="row">
             <nav id="nav"  class="col-md-12">
-            Area nav
-        </nav>
+                Area nav
+            </nav>
         </div>
-        
-		<div class="row">
-			<aside class="col-xs-12 col-sm-3 col-md-3">
-				<ul class="accordion">
-					<li id="one" class="predio">
-						<a href="#one">Predios</a>
-						<ul class="sub-menu">
-							<li><a href="#one">Agregar Nuevo</a></li>
-							<li><a href="#one"><em>02</em>Skydrive<span>87</span></a></li>
-							<li><a href="#one"><em>03</em>FTP Server<span>366</span></a></li>
-						</ul>
-					</li>
-					<li id="two" class="mail">
-						<a href="#two">Mail<span>26</span></a>
-						<ul class="sub-menu">
-							<li><a href="#two"><em>01</em>Hotmail<span>9</span></a></li>
-							<li><a href="#two"><em>02</em>Yahoo<span>14</span></a></li>
-							<li><a href="#two"><em>03</em>Gmail<span>3</span></a></li>
-						</ul>
-					</li>
-					<li id="three" class="cloud">
-						<a href="#three">Cloud<span>58</span></a>
-						<ul class="sub-menu">
-							<li><a href="#three"><em>01</em>Connect<span>12</span></a></li>
-							<li><a href="#three"><em>02</em>Profiles<span>19</span></a></li>
-							<li><a href="#three"><em>03</em>Options<span>27</span></a></li>
-						</ul>
-					</li>
-					<li id="four" class="sign">
-						<a href="#four">Sign Out</a>
-						<ul class="sub-menu">
-							<li><a href="#four"><em>01</em>Log Out</a></li>
-							<li><a href="#four"><em>02</em>Delete Account</a></li>
-							<li><a href="#four"><em>03</em>Freeze Account</a></li>
-						</ul>
-					</li>
-				</ul>
-			</aside>
 
-			<section  class="col-xs-12 col-sm-9 col-md-9">
-				Area section
-				<article class="borde" id="article">
-					Area article
-				</article>
-			</section>
-		</div>
+        <div class="row">
+            <aside class="col-xs-12 col-sm-3 col-md-3">
+                <ul class="accordion">
+                    <li id="one" class="predio">
+                        <a href="#one">Predios</a>
+                        <ul class="sub-menu">
+                            <li><a href="#one">Agregar Nuevo</a></li>
+                            <li><a href="#one"><em>02</em>Skydrive<span>87</span></a></li>
+                            <li><a href="#one"><em>03</em>FTP Server<span>366</span></a></li>
+                        </ul>
+                    </li>
+                    <li id="two" class="mail">
+                        <a href="#two">Mail<span>26</span></a>
+                        <ul class="sub-menu">
+                            <li><a href="#two"><em>01</em>Hotmail<span>9</span></a></li>
+                            <li><a href="#two"><em>02</em>Yahoo<span>14</span></a></li>
+                            <li><a href="#two"><em>03</em>Gmail<span>3</span></a></li>
+                        </ul>
+                    </li>
+                    <li id="three" class="cloud">
+                        <a href="#three">Cloud<span>58</span></a>
+                        <ul class="sub-menu">
+                            <li><a href="#three"><em>01</em>Connect<span>12</span></a></li>
+                            <li><a href="#three"><em>02</em>Profiles<span>19</span></a></li>
+                            <li><a href="#three"><em>03</em>Options<span>27</span></a></li>
+                        </ul>
+                    </li>
+                    <li id="four" class="sign">
+                        <a href="#four">Sign Out</a>
+                        <ul class="sub-menu">
+                            <li><a href="#four"><em>01</em>Log Out</a></li>
+                            <li><a href="#four"><em>02</em>Delete Account</a></li>
+                            <li><a href="#four"><em>03</em>Freeze Account</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </aside>
+
+            <section  class="col-xs-12 col-sm-9 col-md-9">
+                Area section
+                <article class="borde" id="article">
+                    Area article
+                </article>
+            </section>
+        </div>
     </body>       
     <!-- Boostrap core JS -->
 </html>
