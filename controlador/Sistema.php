ea6f0ace52196d4d58addbc920373ce6eba28691
natoehv <nato.ehv@gmail.com>
2014-06-01 13:35:26 -0400

    Carpetas contenedoras del controlador, vista, y modelo

---
+++ controlador/Sistema.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Sistema
+ *
+ * @author Renato
+ */
+class Sistema {
+    //put your code here
+}
+
+?>
---
+++ modelo/PredioDAO.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of PredioDAO
+ *
+ * @author Renato
+ */
+class PredioDAO {
+    //put your code here
+}
+
+?>
