fef5144b12cf9e88219b0aa5360afaf7ed105fee
Javier <Javier@Javier-PC>
2014-05-31 23:13:53 -0400

    Version 1

--- index.php
+++ index.php
@@ -2,21 +2,25 @@
     <head>
         <meta charset="UTF-8">
         <title>Subsistema Patrimonial</title>
-        <link href="css/style.css" rel="stylesheet" type="text/css">
+        <link href="assets/css/style.css" rel="stylesheet" type="text/css">
+        <link rel="shortcut icon" href="/assets/ico/favicon.ico">
+        <!-- Bootstrap core CSS -->
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+
     </head>
     <body>
         <header id="header">
             Area header
         </header>
-        
+
         <nav id="nav">
             Area nav
         </nav>
-        
+
         <aside id="aside">
             Area aside
         </aside>
-        
+
         <section id="section">
             Area section
             <article class="borde" id="article">
