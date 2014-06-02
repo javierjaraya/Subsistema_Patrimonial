880f566de5b78d6c2c6eec9a28a884cb445476b2
natoehv <nato.ehv@gmail.com>
2014-06-02 01:45:46 -0400

    Se agregan datos a clase predio, esta es sólo una clase contenedora (POJO)

--- controlador/Predio.php
+++ controlador/Predio.php
@@ -11,7 +11,81 @@
  * @author Renato
  */
 class Predio {
-    //put your code here
+    private $idPredio;
+    private $nombre;
+    private $superficie;
+    private $estado;
+    private $valorComercial;
+    private $idComuna;
+    private $idZona;
+    private $idEmpresa;
+            
+    function _construct(){}
+    public function getIdPredio() {
+        return $this->idPredio;
+    }
+
+    public function setIdPredio($idPredio) {
+        $this->idPredio = $idPredio;
+    }
+
+    public function getNombre() {
+        return $this->nombre;
+    }
+
+    public function setNombre($nombre) {
+        $this->nombre = $nombre;
+    }
+
+    public function getSuperficie() {
+        return $this->superficie;
+    }
+
+    public function setSuperficie($superficie) {
+        $this->superficie = $superficie;
+    }
+
+    public function getEstado() {
+        return $this->estado;
+    }
+
+    public function setEstado($estado) {
+        $this->estado = $estado;
+    }
+
+    public function getValorComercial() {
+        return $this->valorComercial;
+    }
+
+    public function setValorComercial($valorComercial) {
+        $this->valorComercial = $valorComercial;
+    }
+
+    public function getIdComuna() {
+        return $this->idComuna;
+    }
+
+    public function setIdComuna($idComuna) {
+        $this->idComuna = $idComuna;
+    }
+
+    public function getIdZona() {
+        return $this->idZona;
+    }
+
+    public function setIdZona($idZona) {
+        $this->idZona = $idZona;
+    }
+
+    public function getIdEmpresa() {
+        return $this->idEmpresa;
+    }
+
+    public function setIdEmpresa($idEmpresa) {
+        $this->idEmpresa = $idEmpresa;
+    }
+
+
 }
 
 ?>
