672fbd0723636bbbc47fb773f816ade6a81f94f8
natoehv <nato.ehv@gmail.com>
2014-06-02 02:30:44 -0400

    prototipo 2 js

--- assets/js/eventos.js
+++ assets/js/eventos.js
@@ -9,23 +9,27 @@
  * Propuesta uso de javascript en subsistema_patrimonio
  * @type eventos._L10.Anonym$0
  */
-var eventos = (function() {
-  var variablePublica = "podria iniciar un widget aqui";
 
-  return {
-    /**
-     * 
-     */
-    cargarTabla: function(tabla) {
-      console.log('tabla cargada');
-    },
-    /**
-     * 
-     * @param {type} people
-     * @returns {undefined}
-     */
-    vaciaTabla: function(tabla) {
-      console.log('tabla ocultada');
-    },
-  };
-})();
+
+console.log('iniciando eventos');
+    var eventos = (function() {
+      var variablePublica = "podria iniciar un widget aqui";
+
+      return {
+        /**
+         * 
+         */
+        cargarTabla: function(tabla) {
+          $('section').load('vista/'+tabla+'.php');
+          console.log('tabla cargada');
+        },
+        /**
+         * 
+         * @param {type} people
+         * @returns {undefined}
+         */
+        vaciaTabla: function(tabla) {
+          console.log('tabla ocultada');
+        },
+      };
+    })();
