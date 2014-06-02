029770f3da007d783d11da1ef2675fc6b7a43e34
natoehv <nato.ehv@gmail.com>
2014-06-02 17:50:27 -0400

    Modificación de tabla predios, agregando un formato más estable

--- vista/Predio.php
+++ vista/Predio.php
@@ -8,7 +8,36 @@ include_once '../controlador/Predio.php';
 
 $control = Sistema::getInstancia();
 $predios = $control->findAllPredios();
-foreach($predios as $predio){
-    echo "<h3>".$predio->getNombre()."</h3>";
-}
 ?>
+<div class="row">
+    <h1>Lista de Predios  <button type="button" class="btn btn-primary btn-lg" id="agregarContacto">Nuevo</button></h1>
+</div>
+<div class="row">
+    <table id="tabla_contactos" class='table table-bordered table-hover table-striped tablesorter'>
+        <thead>
+            <tr>
+                <th class="header">ID Predio <i class='fa fa-sort'></i></th>
+                <th class="header">Nombre Predio<i class='fa fa-sort'></i></th>
+                <th class="header">Estado <i class='fa fa-sort'></i></th>
+                <th class="header">Valor Comercial <i class='fa fa-sort'></i></th>
+                <th class="header">Accion</th>
+            </tr>
+        </thead>
+        <tbody id="tbody">
+            <?PHP
+            foreach($predios as $predio){
+                echo "<tr>";
+                echo "<td>".$predio->getIdPredio()."</td>";
+                echo "<td>".$predio->getNombre()."</td>";
+                echo "<td>".$predio->getEstado()."</td>";
+                echo "<td>".$predio->getValorComercial()."</td>";
+                echo "<td>";
+                echo "<button type='button' class='btn btn-warning'>Modificar</button>";
+                echo "<button type='button' class='btn btn-danger'>Eliminar</button>";
+                echo "</td>";
+                echo "</tr>";
+            }
+            ?>
+        </tbody>
+    </table>
+</div>
