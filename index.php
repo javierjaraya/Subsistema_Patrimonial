89674f71df9a5dd8934b5f62b67d85ab05d98a80
natoehv <nato.ehv@gmail.com>
2014-06-03 16:08:28 -0400

    se actualiza eventos por predio.js

--- index.php
+++ index.php
@@ -59,5 +59,5 @@
     <!-- BlockUI core JS -->
     <script type="text/javascript" src="assets/js/jquery.blockUI.js"></script>
     <!-- Eventos subsistema patrimonial JS -->
-    <script type="text/javascript" src="assets/js/eventos.js"></script>
+    <script type="text/javascript" src="assets/js/predio.js"></script>
 </html>
