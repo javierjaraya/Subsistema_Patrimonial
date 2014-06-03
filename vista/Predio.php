f93a39bbcada5189b1b99dc6297018a9f56086e0
natoehv <nato.ehv@gmail.com>
2014-06-02 23:41:42 -0400

    se agrega formulario oculto

--- vista/Predio.php
+++ vista/Predio.php
@@ -10,7 +10,7 @@ $control = Sistema::getInstancia();
 $predios = $control->findAllPredios();
 ?>
 <div class="row">
-    <h1>Lista de Predios  <button type="button" class="btn btn-primary btn-lg" id="agregarContacto">Nuevo</button></h1>
+    <h1>Lista de Predios  <button type="button" class="btn btn-primary btn-lg" id="agregarContacto" onClick="eventos.ingresaNuevoPredio()">Nuevo</button></h1>
 </div>
 <div class="row">
     <table id="tabla_contactos" class='table table-bordered table-hover table-striped tablesorter'>
@@ -41,3 +41,20 @@ $predios = $control->findAllPredios();
         </tbody>
     </table>
 </div>
+
+<div id="nuevoPredio" style="display:none; cursor: default"> 
+        <fieldset>
+            <div><label>Identificador Predio: </label><input type="text" class="idpredio" name="idpredio" /></div>
+            <div><label>Nombre: </label><input type="text" class="nombre" name="nombre" /></div>
+            <div><label>Superficie: </label><input type="text" class="superficie" name="superficie" /></div>
+            <div><label>Valor Comercial:</label><input cols="30" rows="5" class="valorcomercial" name="valorcomercial" /></div>
+            <div><label>Zona: </label><input cols="30" rows="5" class="zona" name="zona" ></input></div>
+            <div><label>Comuna: </label><input cols="30" rows="5" class="comuna" name="comuna" /></div>
+            <div class="ultimo">
+                <img src="assets/ico/ajax.gif" class="ajaxgif hide" />
+                <div class="msg"></div>
+                <button  onclick="eventos.aceptarIngresoPredio()">Enviar Mensaje</button>
+            </div>
+        </fieldset>
+ 
+</div> 
