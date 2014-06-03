622c45e5c95922b16a6037b69ebcf4dea1ad31dc
natoehv <nato.ehv@gmail.com>
2014-06-02 23:41:59 -0400

    se genera archivo el cual guardará datos de predio

---
+++ vista/ingresaPredio.php
@@ -0,0 +1,9 @@
+<?php
+$idpredio = $_POST['idpredio'];
+$nombre = $_POST['nombre'];
+$superficie = $_POST['superficie'];
+$valorcomercial = $_POST['valorcomercial'];
+
+echo $idpredio . " - " .$nombre. " - " .$superficie." - ". $valorcomercial;
+
+?>
