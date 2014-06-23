9429ddb076befe64f18aac8a824272071771255b
natoehv <nato.ehv@gmail.com>
2014-06-23 08:33:24 -0400

    validacion genera reporte inventario

--- vista/inventarioReportes.php
+++ vista/inventarioReportes.php
@@ -2,22 +2,23 @@
 include_once('../controlador/PDF.php');
 include_once '../controlador/Sistema.php';
 
+if(isset($_POST['fechaInicio'])){
+    $fi = $_POST['fechaInicio'];
+    $ff = $_POST['fechaFin'];
+    $idRodal = $_POST['idRodal'];
 
-$fi = $_POST['fechaInicio'];
-$ff = $_POST['fechaFin'];
-$idRodal = $_POST['idRodal'];
- 
-$pdf = new PDF('L');
-$control = Sistema::getInstancia();
-$pdf->AddPage();
+    $pdf = new PDF('L');
+    $control = Sistema::getInstancia();
+    $pdf->AddPage();
 
-$inventarios = $control->findInventarios($idRodal, $fi, $ff);
-$miCabecera = array('Identificador', 'Servicio', 'Sistema Inventario', 'Diáemtro Medio',
-            'Altura Dominante','Área Basal', 'Volumen','N° Árboles', 'Altura');
-     
-$tituloPagina = "Inventarios del Rodal ".$idRodal. " entre las fechas ".$fi." y ".$ff;
+    $inventarios = $control->findInventarios($idRodal, $fi, $ff);
+    $miCabecera = array('Identificador', 'Servicio', 'Sistema Inventario', 'Diáemtro Medio',
+                'Altura Dominante','Área Basal', 'Volumen','N° Árboles', 'Altura');
 
-$pdf->tablaHorizontalInventario($miCabecera, $inventarios,$tituloPagina);
- 
-$pdf->Output();
+    $tituloPagina = "Inventarios del Rodal ".$idRodal. " entre las fechas ".$fi." y ".$ff;
+
+    $pdf->tablaHorizontalInventario($miCabecera, $inventarios,$tituloPagina);
+
+    $pdf->Output();
+}
 ?>
