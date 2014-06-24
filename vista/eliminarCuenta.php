9b1066a1cac4c8240eb63b41adc6d67ed1be8094
Sebas <Sebas@192.168.1.45>
2014-06-24 00:21:19 -0400

    se implemento la funcion eliminar en cuenta.js y CuentaDAO, tambien se agregó eliminarCuenta.php a la carpeta vista (Solo está faltando validar)

--- vista/eliminarCuenta.php
+++ vista/eliminarCuenta.php
@@ -4,8 +4,7 @@ include_once '../controlador/Sistema.php';
 include_once '../controlador/Cuenta.php';
 
 $id = $_POST['idCuenta'];
-echo 1;
-echo $id;
+
 $control = Sistema::getInstancia();
 
 $control->eliminarCuenta($id);
